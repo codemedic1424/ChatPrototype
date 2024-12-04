b873f5e9c2c005b8cfd6dfaace1d381d8cfb7634
codemedic1424 <sunnier.stopper.0x@icloud.com>
2024-12-04 04:57:32 -0500

    First round of placeholder text added.

--- ChatPrototype/ContentView.swift
+++ ChatPrototype/ContentView.swift
@@ -11,8 +11,11 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("Knock, knock!")
-            
+                .padding()
+                .background(Color.yellow, in: RoundedRectangle(cornerRadius: 8))
             Text("Who's there?")
+                .padding()
+                .background(Color.teal, in: RoundedRectangle(cornerRadius: 8))
         }
         .padding()
     }
