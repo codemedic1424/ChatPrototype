5fb6d2f14432d18bec21bee7978cca8ccc4a133f
codemedic1424 <sunnier.stopper.0x@icloud.com>
2024-12-04 03:31:06 -0500

    Initial Commit

---
+++ ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Christopher Wilshusen on 12/4/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
