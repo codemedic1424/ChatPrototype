a07685b0fa6213d66273e9280acf1a1f62bd0cd7
codemedic1424 <sunnier.stopper.0x@icloud.com>
2024-12-07 04:21:20 -0500

    Tutorial Lesson 1 Complete

--- ChatPrototype/ContentView.swift
+++ ChatPrototype/ContentView.swift
@@ -9,13 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack (spacing: 25) {
             Text("Knock, knock!")
                 .padding()
                 .background(Color.yellow, in: RoundedRectangle(cornerRadius: 8))
+                .shadow(radius: 10)
             Text("Who's there?")
                 .padding()
                 .background(Color.teal, in: RoundedRectangle(cornerRadius: 8))
+                .shadow(color: Color.black, radius: 10 )
+            Text("Choo")
+                .padding()
+                .background(Color.yellow, in: RoundedRectangle(cornerRadius: 8))
+            Text("A Choo who?")
+                .padding()
+                .background(Color.teal, in: RoundedRectangle(cornerRadius: 8))
+            Text("Gesungheit!")
+                .padding()
+                .background(Color.yellow, in: RoundedRectangle(cornerRadius: 8))
         }
         .padding()
     }
